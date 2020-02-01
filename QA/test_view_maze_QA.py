ecd58081742bac4ab49c5423c17280959b582a1b
githubbing97 <malcolmtham5566@hotmail.com>
2020-02-01 16:54:22 +0800

    added QA python testcase functions and updated testcases
    As for the QA python testcase functions, i've tried my best to find online for solution and resources but this is the best i can produce for now. Still trying to look for more information and add into the function test cases.

---
+++ QA/test_view_maze_QA.py
@@ -0,0 +1,20 @@
+import pytest
+from MazeCode.mazegame_code import *
+import csv
+
+#viewmaze
+def test_view_the_storemaze():
+    result = "Passed"
+    assert result == ""
+    
+def test_view_storemaze_in_memorylist():
+    result = "Passed"
+    assert result == ""
+    
+def test_reading_maze_from_store_memory():
+    result = "Passed"
+    assert result == ""
+    
+def test_maze_showing_start_end_location():
+    result = "Passed"
+    assert result == ""
