fd8708459a565f687a8ee38b8c5638138205d3f2
Kevin Toh <kevintoh0305@gmail.com>
2020-02-03 16:51:18 +0800

    changed test

--- test_configuremaze.py
+++ test_configuremaze.py
@@ -1,5 +1,4 @@
 import pytest
-# from MazeCode.mazegame import *
 from MazeCode import mazegame
 from MazeCode.mazegame import *
 
@@ -8,7 +7,7 @@ def test_display_configuration_menu():
     assert message == "Displaying Configuration Menu"
      
 def test_no_maze_loaded():
-    mazegame_code.maze = []
+    mazegame.maze = []
     with pytest.raises(GameError) as e:
         createWall(1,1)
     assert "No maze is loaded" in str(e.value)
@@ -16,36 +15,33 @@ def test_no_maze_loaded():
 def test_create_wall():
     check_filename("Maze.csv")
     createWall(1,1)
-    assert mazegame_code.maze[0][0] == "X"
+    assert mazegame.maze[0][0] == "X"
 
 def test_create_passageway():
     check_filename("Maze.csv")
     createPassageWay(2,2)
-    assert mazegame_code.maze[1][1] == "O"
+    assert mazegame.maze[1][1] == "O"
 
 def test_create_start_point():
     check_filename("Maze.csv")
     createStartPoint(2,3)
-    assert mazegame_code.maze[1][2] == "A"
+    assert mazegame.maze[1][2] == "A"
 
 def test_create_end_point():
     check_filename("Maze.csv")
     createEndPoint(5,6)
-    assert mazegame_code.maze[4][5] == "B"
+    assert mazegame.maze[4][5] == "B"
 
 def test_coordinate_input_out_of_range():
     check_filename("Maze.csv")
     with pytest.raises(GameError) as e:
         createPassageWay(-1,-1)
-        assert "Invalid Row Value" in str(e.value)
+    assert "Invalid Row Value" in str(e.value)
 
 def test_return_to_configuration_menu():
     value = handle_configuration_option_input("B", createWall)
     assert value == False
 
 def test_exit_to_main_menu_from_configuration_option():
-    enterCreateWallMenu()
-    message = exitToMainMenu()
-    return message == "Exit to Main Menu"
     value = handle_configuration_option_input("M", createWall)
     assert value == True
